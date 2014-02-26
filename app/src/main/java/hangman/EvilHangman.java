67ceefa24ce075a58f4621d53999c9be866ef27f
Harrie <harrieoosterhuis@gmail.com>
2014-02-26 23:35:18 +0100

    Reimagined evil algorithm for better performance

--- app/src/main/java/hangman/EvilHangman.java
+++ app/src/main/java/hangman/EvilHangman.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
+import nl.mprog.apps.Hangman10196129.database.WordDatabase.WordCursor;
 
 import nl.mprog.apps.Hangman10196129.database.WordDatabase;
 
@@ -17,8 +18,11 @@ public class EvilHangman extends Hangman {
 
     private String state ;
     private int length;
-    private  WordDatabase db ;
     private ArrayList<String> impossibles ;
+
+    private ArrayList<String> possibles ;
+
+    private HashMap<Character, Boolean> guessed ;
 
     public EvilHangman(WordDatabase db, int length, int guesses){
         super(guesses);
@@ -31,20 +35,50 @@ public class EvilHangman extends Hangman {
         this.length = state.length();
         init(db,state);
         updateDisplay(guessed);
+        fill(db);
     }
 
     private void init(WordDatabase db){
-        String startState = "" ;
-        for(int i = 0 ; i < length ;i++){
-            startState += "_" ;
-        }
+        String startState = repeat("_",length);
         init(db, startState);
+        fill(db);
     }
 
     private void init(WordDatabase db, String startState){
-        this.db = db ;
         this.state = startState;
+        this.guessed = new HashMap<Character, Boolean>(26);
+
+        // Creates like syntax for the sql query to ignore already guessed letters
         impossibles = new ArrayList<String>();
+        HashMap<Character,Integer> map = new HashMap<Character, Integer>(26);
+        for(int i = 0; i < state.length() ; i++){
+            char c = state.charAt(i);
+            if(!map.containsKey(c)){
+                map.put(c,1);
+            } else {
+                map.put(c,map.get(c)+1);
+            }
+        }
+        for(Entry<Character,Integer> e : map.entrySet()){
+            if(e.getKey() != '_')
+                impossibles.add(addWildCards(e.getKey(),e.getValue()+1));
+        }
+
+    }
+
+    /**
+     * Fills possible with possibilities.
+     * @param db
+     */
+    private void fill(WordDatabase db){
+        WordCursor c = db.getWordCursor(state,impossibles);
+        possibles = new ArrayList<String>(c.count());
+        c.first();
+        possibles.add(c.getWord().toLowerCase());
+        while(c.hasNext()){
+            possibles.add(c.getWord().toLowerCase());
+            c.next();
+        }
     }
 
     @Override
@@ -52,6 +86,63 @@ public class EvilHangman extends Hangman {
         return state;
     }
 
+    private boolean isGuessed(char c){
+        return guessed.containsKey(c);
+    }
+
+    /**
+     * Returns hangman pattern of given word.
+     * @param word
+     * @return
+     */
+    private String getPattern(String word){
+        String pattern = "";
+        for(int i = 0;i<length;i++){
+            char c = word.charAt(i);
+            if(isGuessed(c))
+                pattern += c ;
+            else
+                pattern += '_';
+        }
+        return pattern;
+    }
+
+    /**
+     * Returns HashMap with pattern as Key and Set of matching words as value
+     * @return
+     */
+    private HashMap<String,ArrayList<String>> splitWords(){
+        HashMap<String,ArrayList<String>> map = new HashMap<String, ArrayList<String>>();
+        for(String word : possibles){
+            String pattern = getPattern(word);
+            if(map.containsKey(pattern)){
+                map.get(pattern).add(word);
+            } else {
+                ArrayList<String> list = new ArrayList<String>();
+                list.add(word);
+                map.put(pattern, list);
+            }
+        }
+        return map ;
+    }
+
+    /**
+     * Sets state to most evil pattern.
+     */
+    private void pickBestPattern(){
+        String old = this.state ;
+        int best = 0 ;
+        HashMap<String,ArrayList<String>> split = splitWords();
+        for(Entry<String, ArrayList<String>> e : split.entrySet()){
+            // Swap if greater size, if equal take the one that makes the guess wrong (more evil :D).
+            if(e.getValue().size() > best ||
+                    (e.getValue().size() == best && old.equals(e.getKey()))){
+                this.state = e.getKey();
+                this.possibles = e.getValue();
+                best = this.possibles.size();
+            }
+        }
+    }
     /**
      * Returns game state, if out of guesses random possibility is chosen.
      * @return State of the game, can be an unfinished game state.
@@ -59,7 +150,7 @@ public class EvilHangman extends Hangman {
     @Override
     public String getSecretWord() {
         if(!guessesLeft())
-            return db.getRandom(state,new ArrayList<String>(),impossibles).toLowerCase();
+            return possibles.get((int) Math.floor(Math.random()*possibles.size()));
         else
             return state ;
     }
@@ -75,103 +166,17 @@ public class EvilHangman extends Hangman {
      */
     @Override
     public void updateDisplay(String multipleGuess) {
-        ArrayList<Character> guessed = new ArrayList<Character>(26);
+        ArrayList<Character> guessList = new ArrayList<Character>(26);
         for(int i = 0; i < multipleGuess.length();i++)
-            if(!guessed.contains(multipleGuess.charAt(i)))
-                guessed.add(multipleGuess.charAt(i));
-        for(char c : guessed)
-            if(state.indexOf(c) < 0)
+            if(!guessList.contains(multipleGuess.charAt(i))){
+                guessList.add(multipleGuess.charAt(i));
+                guessed.put(multipleGuess.charAt(i), true);
+            }
+        for(char c : guessList)
+            if(state.indexOf(c) < 0 && c != '_')
                 impossibles.add(addWildCards(c,1));
     }
 
-    /**
-     * Data structure for selecting the best state.
-     */
-    private class Pair{
-        public String state ;
-        public Long   count ;
-        public int added;
-        public Pair(String state, Long count, int added){
-            this.state = state;
-            this.count = count;
-            this.added = added;
-        }
-        public void set(String state, Long count, int added){
-            this.state = state;
-            this.count = count;
-            this.added = added;
-        }
-    }
-
-    /**
-     * Finds best state to go to after guess.
-     * Best state is the state with the most possible words.
-     * @param guess Guessed letter.
-     * @return Pair structure for the best state to select.
-     */
-    public Pair best(char guess){
-        ArrayList<String> next = new ArrayList<String>();
-        long both = getFullCount(state,next);
-        next.add("%" + String.valueOf(guess) + "%");
-        long without = getFullCount(state,next);
-        long with    = both - without;
-        Log.d("Hangman", "Guess " + guess);
-        Log.d("Hangman", state + " both " + both);
-        Log.d("Hangman", state + " with " + with);
-        Log.d("Hangman", state + " without "+ without);
-        if(with <= without)
-            return new Pair(state,without,0) ;
-        else
-            return bestAdd(state,both,guess,0, new Pair(state,without,0));
-    }
-
-    /**
-     * Returns the best state after placing the guess or not.
-     */
-    public Pair bestAdd(String state, long previous, char guess, int start, Pair best){
-        return  bestAdd(state, previous, guess, start, best, new ArrayList<String>(), 0);
-    }
-    public Pair bestAdd(String state, long previous, char guess, int index, Pair best,
-                        ArrayList<String> unlike, int added){
-        if(index >= length){
-            if(previous > best.count){
-                best.set(state, previous, added);
-            }
-        } else if(state.charAt(index) != '_'){
-            return  bestAdd(state,previous, guess, index+1, best, unlike, added);
-        } else {
-            long both = previous;
-            String cur = state.substring(0,index) + String.valueOf(guess) + state.substring(index+1);
-            // possibilities with a letter added on index
-            long with = getFullCount(cur,unlike);
-            // possibilities without a letter added on index
-            long without = both - with;
-            Log.d("Hangman", cur   + " with " + with);
-            Log.d("Hangman", state + " without "+ without);
-            if(without > best.count){
-                unlike.add(cur);
-                bestAdd(state,without,guess,index+1,best,unlike,added);
-                unlike.remove(unlike.size()-1);
-            }
-            if(with > best.count){
-                unlike.add(addWildCards(guess,added+1));
-                // possibilities without more letters added
-                long limit = getFullCount(cur, unlike);
-                // possibilities with more letters added
-                long expand = with - limit ;
-                Log.d("Hangman", state + " limit " + limit);
-                Log.d("Hangman", state + " expand " + expand);
-                unlike.remove(unlike.size()-1);
-                if(expand > limit)
-                    //explore possibilities when another letter is added
-                    bestAdd(cur,expand,guess,index+1,best,unlike,added+1);
-                else
-                    //stop searching and skip to comparing case
-                    bestAdd(cur,limit,guess,length,best,unlike,added+1);
-            }
-        }
-        return best;
-    }
 
     public String addWildCards(char letter, int times){
         return "%" + repeat(String.valueOf(letter) + "%",times);
@@ -191,28 +196,15 @@ public class EvilHangman extends Hangman {
      */
     @Override
     public synchronized boolean guessLetter(char letter) {
-
-        Pair best = best(letter);
-        if(!this.state.equals(best.state)){
-            this.state = best.state;
-            impossibles.add(addWildCards(letter, best.added+1));
-            return true ;
-        } else
-            impossibles.add("%" + String.valueOf(letter) + "%");
-        return false;
+        guessed.put(letter,true);
+        String old = this.state;
+        pickBestPattern();
+        return !old.equals(this.state);
     }
 
     @Override
     public boolean solved() {
         return state.indexOf("_") < 0 ;
-    }
-
-    /**
-     * Get count of words for a state, without correction meaning (_a__ includes _a_a_)
-     */
-    public long getFullCount(String state, ArrayList<String> unlike){
-        long count = db.count(state, unlike, impossibles);
-        return count ;
     }
 
 
--- app/src/main/java/nl/mprog/apps/Hangman10196129/database/WordDatabase.java
+++ app/src/main/java/nl/mprog/apps/Hangman10196129/database/WordDatabase.java
@@ -65,9 +65,9 @@ public class WordDatabase {
     }
 
     /**
-     * Returns a cursor used for Evil algorithm,words found are like state but unlike given arraylists
+     * Returns a cursor used for Evil algorithm,words found are like state but unlike given arraylist
      */
-    public Cursor getCursor(String state, ArrayList<String> unlike, ArrayList<String> impossibilities) {
+    public Cursor getCursor(String state, ArrayList<String> impossibilities) {
         SQLiteDatabase db = mDbHelper.getReadableDatabase();
 
         String[] projection = {
@@ -80,72 +80,48 @@ public class WordDatabase {
                 " LIKE ?" ;
 
         int incorrectLength = impossibilities.size();
-        int statesCount     = unlike.size();
-        String[] whereArgs = new String[2 + incorrectLength + statesCount];
+        String[] whereArgs = new String[2 + incorrectLength];
         whereArgs[0] = String.valueOf(state.length());
         whereArgs[1] = state;
         for(int i = 0;i < incorrectLength;i++){
             whereClause = whereClause + " AND " + FeedEntry.COLUMN_NAME_WORD  + " NOT LIKE ?";
             whereArgs[2 + i] = impossibilities.get(i) ;
         }
-        for(int i = 0;i < statesCount;i++){
-            whereClause = whereClause + " AND " + FeedEntry.COLUMN_NAME_WORD  + " NOT LIKE ?";
-            whereArgs[2 + incorrectLength + i] = unlike.get(i) ;
-        }
-
         return db.query(FeedEntry.TABLE_NAME, projection, whereClause, whereArgs, null, null, null);
 
     }
 
-    /**
-     * Returns the amount of possibilities
-     * @param state
-     * @param unlike
-     * @param impossibilities
-     * @return
-     */
-    public long count(String state, ArrayList<String> unlike, ArrayList<String> impossibilities) {
-        SQLiteDatabase db = mDbHelper.getReadableDatabase();
-
-        String[] projection = {
-                FeedEntry._ID,
-                FeedEntry.COLUMN_NAME_WORD,
-                FeedEntry.COLUMN_NAME_LENGTH
-        };
-
-        String whereClause = FeedEntry.COLUMN_NAME_LENGTH + " =  ? AND " + FeedEntry.COLUMN_NAME_WORD +
-                " LIKE ?" ;
-
-        int incorrectLength = impossibilities.size();
-        int statesCount     = unlike.size();
-        String[] whereArgs = new String[2 + incorrectLength + statesCount];
-        whereArgs[0] = String.valueOf(state.length());
-        whereArgs[1] = state;
-        for(int i = 0;i < incorrectLength;i++){
-            whereClause = whereClause + " AND " + FeedEntry.COLUMN_NAME_WORD  + " NOT LIKE ?";
-            whereArgs[2 + i] = impossibilities.get(i) ;
-        }
-        for(int i = 0;i < statesCount;i++){
-            whereClause = whereClause + " AND " + FeedEntry.COLUMN_NAME_WORD  + " NOT LIKE ?";
-            whereArgs[2 + incorrectLength + i] = unlike.get(i) ;
-        }
-
-        String rawQuery = "SELECT COUNT(*) FROM " + FeedEntry.TABLE_NAME + " WHERE " +
-                whereClause ;
-
-        Cursor c = db.rawQuery(rawQuery, whereArgs);
-        c.moveToFirst();
-        return c.getLong(0);
+    public WordCursor getWordCursor(String state, ArrayList<String> impossibilities) {
+        return new WordCursor(getCursor(state, impossibilities));
     }
 
-    /**
-     * Returns a random word that fits the restrictions. To make the loser feel bad.
-     * Such Evil is done here.
-     */
-    public String getRandom(String state, ArrayList<String> unlike, ArrayList<String> impossibilities) {
-        Cursor c =  getCursor(state,unlike,impossibilities);
-        c.moveToFirst();
-        c.move((int)Math.floor(Math.random()*c.getCount()));
-        return c.getString(c.getColumnIndex(FeedEntry.COLUMN_NAME_WORD));
+    public static class WordCursor {
+        Cursor cursor;
+
+        public WordCursor(Cursor cursor) {
+            this.cursor = cursor;
+            this.cursor.moveToFirst();
+        }
+
+        public String getWord() {
+            return cursor.getString(cursor.getColumnIndex(FeedEntry.COLUMN_NAME_WORD));
+        }
+
+        public void next() {
+            cursor.moveToNext();
+        }
+
+        public void first() {
+            cursor.moveToFirst();
+        }
+
+        public int count() {
+            return cursor.getCount();
+        }
+
+        public boolean hasNext() {
+            return !cursor.isLast();
+        }
+
     }
 }
